098df0cd6d59698cdba65188ab89c33984c5817e
yuri_sn <yuri.snitko@gmail.com>
2022-11-14 19:39:56 +0300

    ver.1.0

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("btn-submit").onmouseover = function() {
+    this.style.backgroundColor = "#fff";
+}
+
+document.getElementById("btn-submit").onmouseout = function() {
+    this.style.backgroundColor = "hsl(25, 97%, 53%)";
+}
+
+
+
