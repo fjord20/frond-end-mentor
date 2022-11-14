138440061f4e59ebab066312834019c9f0ab0f45
yuri_sn <yuri.snitko@gmail.com>
2022-11-14 20:16:53 +0300

    ver.1.1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-document.getElementById("btn-submit").onmouseover = function() {
+document.getElementById("btnsubmit").onmouseover = function() {
     this.style.backgroundColor = "#fff";
 }
 
-document.getElementById("btn-submit").onmouseout = function() {
+document.getElementById("btnsubmit").onmouseout = function() {
     this.style.backgroundColor = "hsl(25, 97%, 53%)";
 }
 
+document.getElementById("accept").onmouseover = function() {
+    accept.style.color = "hsl(25, 97%, 53%)";
+}
 
+document.getElementById("accept").onmouseout = function() {
+    accept.style.color = "#fff";
+}
 
