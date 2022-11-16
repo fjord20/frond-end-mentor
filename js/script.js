7445dc7012705c0c0ee91c5f667293c75e573a16
yuri_sn <yuri.snitko@gmail.com>
2022-11-16 21:17:56 +0300

    ver.2.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+const rating = document.getElementById('rating');
+const a = document.getElementById('thanks');
+const b = document.getElementById('ratingcard');
+
+/* change background color and text color submit link*/
+
 document.getElementById("btnsubmit").onmouseover = function() {
     this.style.backgroundColor = "#fff";
 }
@@ -13,4 +19,37 @@ document.getElementById("accept").onmouseover = function() {
 document.getElementById("accept").onmouseout = function() {
     accept.style.color = "#fff";
 }
+
+/* change color of numbers */
+
+Array.from(document.querySelectorAll('.number'), function(el){
+    el.onclick = function(){
+        this.style.backgroundColor = "hsl(25, 97%, 53%)";
+        this.style.color = "#fff";
+    }
+})  
+
+/* click on button "SUBMIT"*/
+
+function btnClick() { 
+    if (a.style.display == 'block') 
+        b.style.display = 'none';
+    else 
+        a.style.display = 'block';
+}
+
+/* selected number of vote */
+
+Array.from(document.querySelectorAll('.number')).forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+    });
+})
+
+
+
+
+
+
+
 
